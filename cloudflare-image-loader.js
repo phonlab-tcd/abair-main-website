f9bdabeba07bdd3425c629fdbe2ee92800b89827
john <johnsloan1984@gmail.com>
2023-06-29 11:53:04 +0100

    changes image loader to Cloudflare

---
+++ cloudflare-image-loader.js
@@ -0,0 +1,7 @@
+// Docs: https://developers.cloudflare.com/images/url-format
+export default function cloudflareLoader({ src, width, quality }) {
+  const params = [`width=${width}`, `quality=${quality || 75}`, "format=auto"];
+  return `https://abair-main-website.pages.dev/public/${params.join(
+    ","
+  )}/${src}`;
+}
