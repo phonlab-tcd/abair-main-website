03c69df8a2d6acc570675ae7c0c79e30c81b0432
john <johnsloan1984@gmail.com>
2023-07-06 15:48:45 +0100

    added photos

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 import { supabase } from "@/services/supabase";
 import Link from "next/link";
 
 export default async function Page() {
-  const { data: people } = await supabase.from("people").select();
+  const { data: people } = await supabase
+    .from("people")
+    .select("id, name, image, bio");
 
   if (!people) {
     return <p>No people found.</p>;
@@ -19,6 +22,7 @@ export default async function Page() {
           {people.map((person) => (
             <p key={person.id}>
               <Link href={`/people/${person.name}`}>{person.name}</Link>
+              <img src={person.image} alt={`image of ${person.name}`} />
             </p>
           ))}
         </div>
