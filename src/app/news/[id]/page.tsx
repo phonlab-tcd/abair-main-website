dfd6fd064b703fa9adbbdb7232027fa6ca185205
OscarMBravo <123830369+OscarMBravo@users.noreply.github.com>
2023-08-09 15:54:47 +0100

    changed news layout

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -33,32 +33,33 @@ export default async function Page({ params }: PageProps) {
   }
 
   return (
-    <div className="w-full min-h-screen">
-      <div className="w-full flex justify-center mt-8">
-        <div className="max-w-6xl">
-          <div className="flex-shrink-0 pr-8 w-48">
-            <div className="text-gray-600 text-lg text-center">{news.date}</div>
-            <div className=" px-3 py-2 text-base bg-blue-500 text-white rounded-lg mt-3 text-center">
-              {news.news_category}
+    <div className="bg-white p-4 shadow-md rounded-md flex">
+      <div className="flex-shrink-0 pr-8 w-48 mt-10">
+        <div className="text-gray-600 text-lg text-center">{news.date}</div>
+        <div className="px-3 py-2 text-base bg-blue-500 text-white rounded-lg mt-3 text-center">
+          {news.news_category}
+        </div>
+      </div>
+      <div className="flex-grow">
+        <h1 className="text-3xl font-semibold mb-2 text-center">
+          {news.title_en}
+        </h1>
+        <p className="text-gray-800 mb-4 text-center">{news.blurb_en}</p>
+        <div className="flex space-x-2 justify-center">
+          {news.images.map((image: any, index: number) => (
+            <div key={index} className="object-cover rounded-md">
+              <Image
+                src={image.url}
+                alt={image.url.toString()}
+                height={250}
+                width={375}
+              />
             </div>
-          </div>
-          <div className="flex-grow">
-            <h1 className="text-3xl font-semibold mb-2">{news.title_en}</h1>
-            <p className="text-gray-800 mb-4">{news.blurb_en}</p>
-            <div className="flex space-x-2">
-              {news.images.map((image: any, index: number) => (
-                <div key={index} className="w-48 h-48 object-cover rounded-md">
-                  <Image
-                    src={image.url}
-                    alt={image.toString()}
-                    height={200}
-                    width={300}
-                  />
-                </div>
-              ))}
-            </div>
-            <div className="text-gray-800">{news.body_en}</div>
-          </div>
+          ))}
+        </div>
+
+        <div className="text-gray-800" style={{ whiteSpace: "pre-line" }}>
+          {news.body_en.replace(/~/g, "\n")}
         </div>
       </div>
     </div>
