8b34a604bd7afd034d972af6ea6278bcdfbb5087
OscarMBravo <123830369+OscarMBravo@users.noreply.github.com>
2023-10-05 12:06:42 +0100

    green background added and width set to full

--- src/app/[lng]/applications/components/AppCard.tsx
+++ src/app/[lng]/applications/components/AppCard.tsx
@@ -26,29 +26,31 @@ const AppCard = ({
 
   return (
     <div
-      className={`border ${
+      /*className={`border ${
         isDisabled ? "bg-gray-300" : "border-primary-200 border-2"
       } p-4 ${
         isDisabled ? "" : "cursor-pointer hover:bg-primary-200 duration-500"
-      } relative`}
+      } */
+      className="relative w-[300px] h-[300px] bg-primary-500 p-1 my-4 mx-2"
     >
       <a
         href={isDisabled ? undefined : url}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <div className="flex space-x-4">
+        <div className="flex bg-primary-500 bg-blend-normal">
           <Image
             src={image}
             alt={name}
-            width={100}
-            height={80}
-            className="w-[100px] h-[80px] object-cover rounded-md md:w-[175px] md:h-[140px] lg:w-[250px] lg:h-[200px]"
+            width={1080}
+            height={500}
+            //className="w-full h-[150px] object-cover rounded-md md:w-[175px] md:h-[140px] lg:w-[250px] lg:h-[200px]"
+            className="w-full h-[150px] object-cover"
           />
-          <div className="flex-grow">
-            <h2 className="text-lg font-semibold">{name}</h2>
-            <p className="text-gray-800">{description}</p>
-          </div>
+        </div>
+        <div className="flex-grow">
+          <h2 className="text-lg text-white font-semibold">{name}</h2>
+          <p className="text-white">{description}</p>
         </div>
       </a>
       {isDisabled && (
