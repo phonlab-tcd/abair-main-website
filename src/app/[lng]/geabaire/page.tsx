f1843c809a9b876c65a7769dffce24f35fe82ef8
Rían Errity <rian@paradaux.io>
2023-12-20 20:44:57 +0000

    Remove problematic page

--- src/app/[lng]/geabaire/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import { useRouter } from "next/navigation"
-
-export default function Geabaire(props: any) {
-  const router = useRouter();
-  router.push("https://geabaire.abair.ie");
-}
